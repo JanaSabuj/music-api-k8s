478a3d4311e6957050cbafb2d28ad6407dc38e7b
sabuj.jana <sabuj.jana@flipkart.com>
2023-03-12 11:59:41 +0530

    mysql manifests created

--- pkg/db/sql.sh
+++ pkg/db/sql.sh
@@ -2,7 +2,8 @@
 create database evergreen_music_db;
 
 # insert into table songs
-INSERT INTO songs (name, artist) VALUES ("Hotel California", "The Eagles");
-INSERT INTO songs (name, artist) VALUES ("Sound of Silence", "Simon and Garfunkel");
+INSERT INTO songs (name, artist) VALUES ("Burn It Down", "Linkin Park");
 INSERT INTO songs (name, artist) VALUES ("Earth Song", "Michael Jackson");
 INSERT INTO songs (name, artist) VALUES ("Hey Jude", "The Beatles");
+INSERT INTO songs (name, artist) VALUES ("Sound of Silence", "Simon and Garfunkel");
+INSERT INTO songs (name, artist) VALUES ("Hotel California", "The Eagles");
