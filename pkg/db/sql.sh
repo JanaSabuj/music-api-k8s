6ada3cbbe28f4b6ed260922907e6f38509f10de1
sabuj.jana <sabuj.jana@flipkart.com>
2023-03-13 22:44:08 +0530

    Updated sql.sh

--- pkg/db/sql.sh
+++ pkg/db/sql.sh
@@ -2,8 +2,9 @@
 create database evergreen_music_db;
 
 # insert into table songs
-INSERT INTO songs (name, artist) VALUES ("Burn It Down", "Linkin Park");
-INSERT INTO songs (name, artist) VALUES ("Earth Song", "Michael Jackson");
-INSERT INTO songs (name, artist) VALUES ("Hey Jude", "The Beatles");
-INSERT INTO songs (name, artist) VALUES ("Sound of Silence", "Simon and Garfunkel");
-INSERT INTO songs (name, artist) VALUES ("Hotel California", "The Eagles");
+INSERT INTO songs (created_at, updated_at, name, artist) VALUES (now(), now(), "Burn It Down", "Linkin Park");
+INSERT INTO songs (created_at, updated_at, name, artist) VALUES (now(), now(), "Earth Song", "Michael Jackson");
+INSERT INTO songs (created_at, updated_at, name, artist) VALUES (now(), now(), "Hey Jude", "The Beatles");
+INSERT INTO songs (created_at, updated_at, name, artist) VALUES (now(), now(), "Sound of Silence", "Simon and Garfunkel");
+INSERT INTO songs (created_at, updated_at, name, artist) VALUES (now(), now(), "Hotel California", "The Eagles");
+INSERT INTO songs (created_at, updated_at, name, artist) VALUES (now(), now(), "Comfortably Numb", "Pink Floyd");
